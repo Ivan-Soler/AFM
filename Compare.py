fec1349861253cfa73303d4e9853413409c464eb
Ivan Soler <ivan.soler.calero@uni-jena.de>
2023-09-26 12:47:25 +0200

    Refactored and simplified the analysis for the GM and the index theorem

---
+++ Compare.py
@@ -0,0 +1,328 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import Read
+import importlib
+import analyzer
+import re
+import pickle
+plt.rcParams.update({'font.size': 16})
+#plt.rcParams['text.usetex'] = True
+                
+    
+def Construct_susy(susy_read_s0,susy_read_s1,conf): #It assumes eigenvalues are ordered
+    
+    #Read supersymmetric modes up to a threshold of the eigenvalue
+    density_susy=np.zeros(sizes[3])
+    read=False
+    for mode in range(0,susy_read_s1):
+        read=True
+        Mode = folder+"sector_1/SusyMode_bin_"+str(mode)+"-"+str(conf)
+        density_s1,sizes=Read.bin_mode_1d(Mode,sizes,colors,spin_length)
+        density_susy+=density_s1
+        
+    for mode in range(0,susy_read_s0):
+        read=True
+        Mode = folder+"sector_0/SusyMode_bin_"+str(mode)+"-"+str(conf)
+        density_s1,sizes=Read.bin_mode_1d(Mode,sizes,colors,spin_length)
+        density_susy-=density_s0
+        
+    #If there is no eigenvalue below the threshold we get the lowest one    
+    if not read:
+        ev1=float(dictionary_s1[str(conf)][0])
+        ev2=float(dictionary_s0[str(conf)][0])
+        if ev1<ev2:
+            read=True
+            Mode = folder+"sector_1/SusyMode_bin_"+str(0)+"-"+str(conf)
+            density_s1,sizes=Read.bin_mode_1d(Mode,sizes,colors,spin_length)
+            density_susy+=density_s1
+        else:
+            read=True
+            Mode = folder+"sector_0/SusyMode_bin_"+str(0)+"-"+str(conf)
+            density_s0,sizes=Read.bin_mode_1d(Mode,sizes,colors,spin_length)
+            density_susy+=-density_s0
+    return(density_susy)   
+
+def Construct_susy_improve(modes_s0, modes_s1,top):
+    GM_00=Compare.GM_matrix(modes_s0,modes_s0)
+    GM_01=Compare.GM_matrix(modes_s0,modes_s1)
+    GM_11=Compare.GM_matrix(modes_s1,modes_s1)
+    
+    modes_s0_used=np.full((len(modes_s0),False))
+    modes_s1_used=np.full((len(modes_s1),False))
+    for i in range(0,len(modes_s0)):        
+        for j in range(0,i):
+            if GM_00[i,j]>0.9:
+                repeated=True
+        if not repeated:
+            susy-=modes_s0
+            modes_s0_used[i]=True
+                
+    for i in range(0,len(modes_s1)):        
+        for j in range(0,i):
+            if GM_11[i,j]>0.9:
+                repeated=True
+        if not repeated:
+            susy+=modes_s1
+            modes_s1_used[i]=True
+                
+    for i in range(0,len(modes_s1)):        
+        for j in range(0,i):
+            if GM_01[i,j]>0.9:
+                repeated=True
+        if repeated:
+            if top[Maxima_find.simple_1d(modes_s1,sizes)]<0:
+                susy-=modes_s1
+                modes_s1_used[i]=False
+            else:
+                susy+=modes_s0
+                modes_s0_used[i]=False
+    return(susy, modes_s0_used, modes_s1_used)   
+
+def GM_RPO_cut(folder,sizes,max_modes,colors,spin_length,configurations,lambdas,RPO_threshold):
+    
+    #Param and definitions
+    conf_tot=len(configurations)
+    steps=len(lambdas)
+    GM_tot=np.zeros((steps))
+    RPO_tot=np.zeros((steps))      
+    param=np.zeros((steps))
+    
+    #Parsing the eigenvalues and finding the end of the spectrum
+    dictionary_s1=Real_eigenvalue(folder+"./sector_1/Measure.seq")
+    dictionary_s0=Real_eigenvalue(folder+"./sector_0/Measure.seq")
+
+    ov_top_dif,susy_top_dif,conf_read=analyzer.Topology_dic(folder+"../gf_afm_4p0t/",0.175,configurations) #To know the good configurations, susy tot is a dictionary with the information of the configurations being used
+    
+    ov_max=0
+    susy_max=0 #To know in which step of lambda one runs out of values
+    k=0
+    for thresholds in lambdas:
+        #Check how many modes for each configuration we need to read 
+        susy_read_s0=analyzer.Count_index(folder,"sector_0/Measure.seq",threshold,conf_read)
+        susy_read_s1=analyzer.Count_index(folder,"sector_0/Measure.seq",threshold,conf_read)
+        GM={}
+        RPO={}
+        j=0
+        
+        for conf in conf_read:
+            #Read GF
+            Topology =folder+"../gf/profile4dt4c"+str(conf)+"to.dat"
+            density_top,sizes=Read.topology_1d(Topology)
+            
+            #Construct susy mode
+            density_susy=Construct_susy(susy_read_s0[conf],susy_read_s1[conf],conf)
+            
+            #Compute the distance between the susy and the topological density
+            GM[conf]=analyzer.Geom_mean_1d(density_susy,density_top)
+            RPO[conf]=analyzer.RPO(np.absolute(density_susy),np.absolute(density_top),RPO_threshold)
+            
+
+        #Store the means        
+        GM_mean=0
+        RPO_mean=0
+        count_meas=0
+        for key in GM:
+            GM_mean+=GM[key]
+            RPO_mean+=RPO[key]
+            count_meas+=1
+        RPO_mean=RPO_mean/count_meas  
+        GM_mean=GM_mean/count_meas
+        GM_tot[k]=GM_mean
+        RPO_tot[k]=RPO_mean
+        print(k) #to see the progress
+        k+=1
+        
+        #To check when one runs out of modes
+        end_overlap, end_susy=analyzer.End_spectrum(folder) #Computes the last value of the spectrum
+        i=0
+        for threshold in lambdas:
+            if not ov_max and threshold > end_overlap: #Computes on which step
+                ov_max=[threshold,i]
+            if not susy_max and threshold > end_susy:
+                susy_max=[threshold,i]
+            i+=1
+        
+    #Plotting and saving the GM and RPO
+    np.savetxt(folder+"./end_spectrum.txt", np.vstack((end_overlap,end_susy)))
+    np.savetxt(folder+"./GM.txt",np.vstack((GM_tot,lambdas)))
+    np.savetxt(folder+"./RPO.txt", np.vstack((RPO_tot,lambdas)))
+
+
+def Index_dic(folder,lambdas,configurations):
+
+    steps=len(lambdas)
+    ov_dif_count=np.zeros((steps))
+    susy_dif_count=np.zeros((steps)) 
+    ov_dif_mean=np.zeros((steps))
+    susy_dif_mean=np.zeros((steps)) 
+    ov_max=0
+    susy_max=0
+
+    #Calculating where do you run out of eigenvalues
+    end_overlap, end_susy=analyzer.End_spectrum(folder)
+    
+    #index configurations
+    for threshold in lambdas:
+        ov_top_dif, susy_top_dif, conf_tot = analyzer.Topology_dic_impr(folder,threshold,configurations)
+        for key in ov_top_dif:
+            ov_dif_mean[i]+=abs(ov_top_dif[key])
+            if abs(ov_top_dif[key]>0.1): ov_dif_count[i]+=1
+        for key in susy_top_dif:
+            susy_dif_mean[i]+=abs(susy_top_dif[key])
+            if abs(susy_top_dif[key]>0.1): susy_dif_count[i]+=1
+            
+        #Adding the step and the lambda where you run out of eigenmodes
+        if not ov_max and threshold > end_overlap:
+            ov_max=[threshold,i]
+        if not susy_max and threshold > end_susy:
+            susy_max=[threshold,i]
+    
+    #Saving the results
+    np.savetxt(folder+"./end_spectrum.txt", np.vstack((ov_max,susy_max)))
+    np.savetxt(folder+"./Index_ov.txt", np.vstack((ov_dif_count/conf_tot,param)))
+    np.savetxt(folder+"./Top_ov.txt", np.vstack((ov_dif_mean,param)))
+    np.savetxt(folder+"./Index_susy.txt",  np.vstack((susy_dif_count/conf_tot,param)))
+    np.savetxt(folder+"./Top_susy.txt", np.vstack((susy_dif_mean,param)))
+    np.savetxt(folder+"./Top_susy.txt", np.vstack((susy_dif_mean,param)))
+    
+    with open('used_conf.pkl', 'wb') as f:
+        pickle.dump(susy, f)
+    return()
+
+def GF_vs_GF(folder,conf_start, conf_end, conf_step,t_start,t_end,t_step,RPO_threshold):
+    
+    steps=int((t_end-t_start)/t_step)
+    tot_conf=int((conf_end-conf_start)/conf_step)
+    GM_tot=np.zeros((steps+1))
+    RPO_tot=np.zeros((steps+1))
+    
+    k=0
+    ov_top_dif,susy_top_dif,conf_tot,susy_tot=analyzer.Topology_dic(folder+"../gf_afm_4p0t/",0.175)
+    for i in range(0, steps+1):
+        t=t_start+t_step*i
+        if int(t)==t: t=int(t)
+        GM={}
+        RPO={}
+        tot_conf=0
+        for conf in range(conf_start, conf_end, conf_step):
+            if susy_tot[str(conf)]: 
+                Topology_smooth=folder+"profile4dt4c"+str(conf)+"to.dat"
+                density_smooth,sizes=Read.topology_1d(Topology_smooth)
+
+                Topology_t=folder+"profile4dt"+str(t)+"c"+str(conf)+"to.dat"
+                density_t,sizes=Read.topology_1d(Topology_t)
+
+                GM[conf]=analyzer.Geom_mean_1d(density_t,density_smooth)
+                RPO[conf]=analyzer.RPO(np.absolute(density_t),np.absolute(density_smooth),RPO_threshold)           
+                tot_conf+=1
+        GM_mean=0
+        RPO_mean=0
+        for key in GM:
+            GM_mean+=GM[key]
+            RPO_mean+=RPO[key]
+        GM_tot[k]=GM_mean/tot_conf
+        RPO_tot[k]=RPO_mean/tot_conf
+        k+=1
+        print(tot_conf)
+    param=np.arange(t_start, t_end+t_step, t_step)
+    np.savetxt(folder+"./GM.txt",np.vstack((GM_tot,param)))
+    np.savetxt(folder+"./RPO.txt", np.vstack((RPO_tot,param)))  
+    
+    return(GM)
+
+
+def Xi(densityA,densityB):
+    Xi=0
+    meanA=np.mean(densityA)
+    meanB=np.mean(densityB)
+    sizes=[len(densityA[0]),len(densityA[1]),len(densityA[2]),len(densityA[3])]
+    volume=sizes[0]*sizes[1]*sizes[2]*sizes[3]
+    meanA=np.mean(densityA)
+    meanB=np.mean(densityB)
+    for x in range(sizes[0]):
+        for y in range(sizes[1]):
+            for z in range(sizes[2]):
+                for t in range(sizes[3]):
+                    Xi+=((densityA[x,y,z,t]-meanA)*(densityB[x,y,z,t]-meanB))/volume
+    return Xi
+
+def Geom_mean(densityA,densityB):
+    GM=(Xi(densityA,densityB)*Xi(densityA,densityB))/(Xi(densityA,densityA)*Xi(densityB,densityB))
+    
+    return GM
+
+def Xi_1d(densityA,densityB):
+    Xi=0
+    meanA=np.mean(densityA)
+    meanB=np.mean(densityB)
+    sizes=[len(densityA)]
+    volume=sizes[0]
+    meanA=np.mean(densityA)
+    meanB=np.mean(densityB)
+    for t in range(sizes[0]):
+        Xi+=((densityA[t]-meanA)*(densityB[t]-meanB))/volume
+    return Xi
+
+def Geom_mean_1d(densityA,densityB):
+    GM=(Xi_1d(densityA,densityB)*Xi_1d(densityA,densityB))/(Xi_1d(densityA,densityA)*Xi_1d(densityB,densityB))
+    
+    return GM
+
+def IPR(density):
+    IPR=0
+    for i in range(0,len(density)):
+        IPR+=density[i]*density[i]
+    return(len(density)*IPR)
+
+
+def RPO(densityA,densityB,thresholdA):
+    volA=0
+    volB=0
+    vol_union=0
+    vol_inter=0
+    for element in densityA:
+        if element > thresholdA:
+            volA+=1
+    if volA==0:
+        return(0)
+    stop=0
+    stop_max=10000
+    thresholdB=thresholdA
+    while (volA!=volB) and (stop<stop_max): 
+        volB=0
+        stop+=1
+        for element in densityB:
+            if element > thresholdB:
+                volB+=1
+        thresholdB-=0.01
+    thresholdB+=0.01    
+    if (stop<stop_max):
+        for t in range(0,len(densityA)):
+            if (densityA[t]>thresholdA) and (densityB[t]>thresholdB):
+                vol_inter+=1
+            if (densityA[t]>thresholdA) or (densityB[t]>thresholdB):
+                vol_union+=1  
+        return (vol_inter/vol_union)
+    else:
+        trehsoldB=thresholdA
+        stop=0
+        while (volA!=volB) and (stop<1000):
+            
+            volB=0
+            stop+=1
+            for element in densityB:
+                if element > thresholdB:
+                    volB+=1
+            thresholdB+=0.01 
+        thresholdB-=0.01    
+        if (stop<stop_max):
+            for t in range(0,len(densityA),1):
+                if (densityA[t]>thresholdA) and (densityB[t]>thresholdB):
+                    vol_inter+=1
+                if (densityA[t]>thresholdA) or (densityB[t]>thresholdB):
+                    vol_union+=1   
+            return (vol_inter/vol_union)
+        
+        else:
+            print("RPO volume did not converged")
+            return (0)
--- analyzer.py
+++ analyzer.py
@@ -1,119 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d import axes3d, Axes3D
-from scipy import interpolate
-from scipy.interpolate import RegularGridInterpolator
-from scipy.interpolate import interpn
-import Projecting
-import Plotting
 import Read
-import Maxima_find
-import Fitting
-import subprocess
 import math
 import re
-from subprocess import call
-
-plt.rcParams.update({'font.size': 12})
-
-    return density, param
-
-def Xi(densityA,densityB):
-    Xi=0
-    meanA=np.mean(densityA)
-    meanB=np.mean(densityB)
-    sizes=[len(densityA[0]),len(densityA[1]),len(densityA[2]),len(densityA[3])]
-    volume=sizes[0]*sizes[1]*sizes[2]*sizes[3]
-    meanA=np.mean(densityA)
-    meanB=np.mean(densityB)
-    for x in range(sizes[0]):
-        for y in range(sizes[1]):
-            for z in range(sizes[2]):
-                for t in range(sizes[3]):
-                    Xi+=((densityA[x,y,z,t]-meanA)*(densityB[x,y,z,t]-meanB))/volume
-    return Xi
-
-def Geom_mean(densityA,densityB):
-    GM=(Xi(densityA,densityB)*Xi(densityA,densityB))/(Xi(densityA,densityA)*Xi(densityB,densityB))
-    
-    return GM
-
-def Xi_1d(densityA,densityB):
-    Xi=0
-    meanA=np.mean(densityA)
-    meanB=np.mean(densityB)
-    sizes=[len(densityA)]
-    volume=sizes[0]
-    meanA=np.mean(densityA)
-    meanB=np.mean(densityB)
-    for t in range(sizes[0]):
-        Xi+=((densityA[t]-meanA)*(densityB[t]-meanB))/volume
-    return Xi
-
-def Geom_mean_1d(densityA,densityB):
-    GM=(Xi_1d(densityA,densityB)*Xi_1d(densityA,densityB))/(Xi_1d(densityA,densityA)*Xi_1d(densityB,densityB))
-    
-    return GM
-
-def IPR(density):
-    IPR=0
-    for i in range(0,len(density)):
-        IPR+=density[i]*density[i]
-    return(len(density)*IPR)
 
 
-def RPO(densityA,densityB,thresholdA):
-    volA=0
-    volB=0
-    vol_union=0
-    vol_inter=0
-    for element in densityA:
-        if element > thresholdA:
-            volA+=1
-    if volA==0:
-        return(0)
-    stop=0
-    stop_max=10000
-    thresholdB=thresholdA
-    while (volA!=volB) and (stop<stop_max): 
-        volB=0
-        stop+=1
-        for element in densityB:
-            if element > thresholdB:
-                volB+=1
-        thresholdB-=0.01
-    thresholdB+=0.01    
-    if (stop<stop_max):
-        for t in range(0,len(densityA)):
-            if (densityA[t]>thresholdA) and (densityB[t]>thresholdB):
-                vol_inter+=1
-            if (densityA[t]>thresholdA) or (densityB[t]>thresholdB):
-                vol_union+=1  
-        return (vol_inter/vol_union)
-    else:
-        trehsoldB=thresholdA
-        stop=0
-        while (volA!=volB) and (stop<1000):
-            
-            volB=0
-            stop+=1
-            for element in densityB:
-                if element > thresholdB:
-                    volB+=1
-            thresholdB+=0.01 
-        thresholdB-=0.01    
-        if (stop<stop_max):
-            for t in range(0,len(densityA),1):
-                if (densityA[t]>thresholdA) and (densityB[t]>thresholdB):
-                    vol_inter+=1
-                if (densityA[t]>thresholdA) or (densityB[t]>thresholdB):
-                    vol_union+=1   
-            return (vol_inter/vol_union)
-        
-        else:
-            print("RPO volume did not converged")
-            return (0)
-        
+plt.rcParams.update({'font.size': 12})        
+
+# Reads the eigenvalue of the supersymmetric operator of each mode
+def Real_eigenvalue(file):
+    Measure_file = open(file, "r")
+    pattern=":OverlapFilterModeR:"
+    eigenvectors=[[]]
+    count=[[]]
+    eigenvectors.pop(0)
+    count={}
+    dictionary={}
+    for line in Measure_file:
+        if re.search(pattern,line):
+            line_split=line.split(":")
+            if (float(line_split[8])<1):
+                eigenvectors.append([line_split[1],line_split[4],line_split[8]])
+                if line_split[1] in dictionary:
+                    dictionary[line_split[1]].append(line_split[8])
+                    count[line_split[1]]+=1
+                else:
+                    dictionary.update({line_split[1]:[line_split[8]]})
+                    count.update({line_split[1]:1})
+    return dictionary
+
+def GM_matrix(modes1, modes2):
+    GM=np.zeros((len(modes1),len(modes2)))
+    for i in range(0,len(modes1)):
+        for j in range(0,len(modes2)):
+            GM[i,j]=analyzer.Geom_mean_1d(modes1(i),modes2(j))
+    return(GM)
+
 def End_spectrum(folder):
     #Overlap
     end_overlap=100
@@ -178,18 +100,20 @@ def End_spectrum(folder):
     return(end_overlap,end_susy)
  
 
-def Count_index(folder,measurement,threshold):
+def Count_index(folder,measurement,threshold,configurations):
     count={}
     with open(folder) as file:
         for line in file:
             match=re.search(measurement,line)
             if match:
                 string=line.split(":")
-                if (abs(float(string[8]))<threshold):
-                    if string[1] in count:
-                        count[string[1]]+=1
-                    else:
-                        count[string[1]]=1
+                for conf in configurations:
+                    if (abs(float(string[8]))<threshold) and string[1]==str(conf):
+                        if string[1] in count:
+                            count[string[1]]+=1
+                        else:
+                            count[string[1]]=1
+                    break
     return(count)
 
 def Count_index_impr(folder,measurement,threshold,modes_used):
@@ -199,69 +123,83 @@ def Count_index_impr(folder,measurement,threshold,modes_used):
             match=re.search(measurement,line)
             if match:
                 string=line.split(":")
-                if (abs(float(string[8]))<threshold) and modes_used[string[7]]:
-                    if string[1] in count:
-                        count[string[1]]+=1
-                    else:
-                        count[string[1]]=1
+                for conf in configurations:
+                    if (abs(float(string[8]))<threshold) and modes_used[string[7]] and string[1]==str(conf):
+                        if string[1] in count:
+                            count[string[1]]+=1
+                        else:
+                            count[string[1]]=1
+                        break
     return(count)
 
-def Topology_dic(folder,threshold):
+def Round_half_integers(number):
+    return(round(number*2)/2)
 
-    #Overlap
-    count_s0_ov=Count_index(folder+"sector_0/Measure.seq", ":OverlapFilterModeC:",threshold)
-    count_s1_ov=Count_index(folder+"sector_1/Measure.seq", ":OverlapFilterModeC:",threshold)
-    count_s0_susy=Count_index(folder+"sector_0/Measure.seq", ":OverlapFilterModeR:",threshold,)
-    count_s1_susy=Count_index(folder+"sector_1/Measure.seq", ":OverlapFilterModeR:",threshold)
-    #GF
-    top_gauge={}
-    susy_top_tot={}
+def Count_index_gf(folder,configurations):
+    count_gauge={}
     with open(folder+"../gf/Measure.seq") as file:
         for line in file:
             match=re.search(":TopologicalCharge:t:4",line)
             if match:
                 string=line.split(":")
-                top_gauge[string[1]]=round((float(string[7])))
-    for key in top_gauge:
+                for conf in configurations: #check that the configuration is on the list of configurations to be read
+                    if string[1]==str(conf):
+                        count_gauge[string[1]]=round(float(string[7]))
+                        break
+    return(count_gauge)
+
+def Topology_dic(folder,threshold,configurations):
+
+    #Overlap
+    count_s0_ov=Count_index(folder+"sector_0/Measure.seq", ":OverlapFilterModeC:",threshold,configurations)
+    count_s1_ov=Count_index(folder+"sector_1/Measure.seq", ":OverlapFilterModeC:",threshold,configurations)
+    #Real
+    count_s0_susy=Count_index(folder+"sector_0/Measure.seq", ":OverlapFilterModeR:",threshold,configurations)
+    count_s1_susy=Count_index(folder+"sector_1/Measure.seq", ":OverlapFilterModeR:",threshold,configurations)
+    #GF
+    count_gauge=Count_index_gf(folder,configurations)
+    #Create the dictionary with the susy 
+    conf_read={}
+    for key in count_gauge: #to create an element in each dictionary with the counting in case they are empty for the given threshold
         if key not in count_s0_ov: count_s0_ov[key]=0
         if key not in count_s1_ov: count_s1_ov[key]=0
         if key not in count_s0_susy: count_s0_susy[key]=0
         if key not in count_s1_susy: count_s1_susy[key]=0
-    conf_tot=len(top_gauge)
-    #for key in top_gauge: print((count_s1_ov[key] + count_s0_ov[key]))
-    ov_top_dif={key: (count_s1_ov[key] - count_s0_ov[key])/4. - top_gauge[key] for key in top_gauge}
-    susy_top_dif={key: (count_s1_susy[key] - count_s0_susy[key])/2. - top_gauge[key] for key in top_gauge}
+
+    #for key in count_gauge: print((count_s1_ov[key] + count_s0_ov[key]))
+    ov_top_dif={key: (count_s1_ov[key] - count_s0_ov[key])/4. - count_gauge[key] for key in count_gauge}
+    susy_top_dif={key: (count_s1_susy[key] - count_s0_susy[key])/2. - count_gauge[key] for key in count_gauge}
     
-    for key in top_gauge:
+    for key in count_gauge:
         if count_s0_susy[key]!=0 and count_s1_susy[key]!=0 :
-            susy_top_tot[key]=True
+            conf_read[key]=True
         else :
-            susy_top_tot[key]=False
-    return(ov_top_dif,susy_top_dif,conf_tot,susy_top_tot)
+            conf_read[key]=False
+    return(ov_top_dif,susy_top_dif,conf_read)
     
 def Topology_dic_impr(folder,threshold,modes_used_s0,modes_used_s1):
 
     #Overlap
-    count_s0_ov=Count_index(folder+"sector_0/Measure.seq", ":OverlapFilterModeC:",threshold)
-    count_s1_ov=Count_index(folder+"sector_1/Measure.seq", ":OverlapFilterModeC:",threshold)
-    count_s0_susy=Count_index(folder+"sector_0/Measure.seq", ":OverlapFilterModeR:",threshold,modes_used_s0)
-    count_s1_susy=Count_index(folder+"sector_1/Measure.seq", ":OverlapFilterModeR:",threshold,modes_used_s1)
+    count_s0_ov=Count_index(folder+"sector_0/Measure.seq", ":OverlapFilterModeC:",configurations,threshold)
+    count_s1_ov=Count_index(folder+"sector_1/Measure.seq", ":OverlapFilterModeC:",configurations,threshold)
+    count_s0_susy=Count_index_impr(folder+"sector_0/Measure.seq", ":OverlapFilterModeR:",configurations,threshold,modes_used_s0)
+    count_s1_susy=Count_index_impr(folder+"sector_1/Measure.seq", ":OverlapFilterModeR:",configurations,threshold,modes_used_s1)
     #GF
-    top_gauge={}
+    count_gauge={}
     with open(folder+"../gf/Measure.seq") as file:
         for line in file:
             match=re.search(":TopologicalCharge:t:4",line)
             if match:
                 string=line.split(":")
-                top_gauge[string[1]]=round((float(string[7])))
-    for key in top_gauge:
+                count_gauge[string[1]]=round((float(string[7])))
+    for key in count_gauge:
         if key not in count_s0_ov: count_s0_ov[key]=0
         if key not in count_s1_ov: count_s1_ov[key]=0
         if key not in count_s0_susy: count_s0_susy[key]=0
         if key not in count_s1_susy: count_s1_susy[key]=0
-    conf_tot=len(top_gauge)
-    #for key in top_gauge: print((count_s1_ov[key] + count_s0_ov[key]))
-    ov_top_dif={key: (count_s1_ov[key] - count_s0_ov[key])/4. - top_gauge[key] for key in top_gauge}
-    susy_top_dif={key: (count_s1_susy[key] - count_s0_susy[key])/2. - top_gauge[key] for key in top_gauge}
+    conf_tot=len(count_gauge)
+    #for key in count_gauge: print((count_s1_ov[key] + count_s0_ov[key]))
+    ov_top_dif={key: (count_s1_ov[key] - count_s0_ov[key])/4. - count_gauge[key] for key in count_gauge}
+    susy_top_dif={key: (count_s1_susy[key] - count_s0_susy[key])/2. - count_gauge[key] for key in count_gauge}
     
     return(ov_top_dif,susy_top_dif,conf_tot)
