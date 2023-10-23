54a8ba262a565086fa6bd969547ad975f9bfa3ce
Ivan Soler <ivsol@pc77.tpi.uni-jena.de>
2023-10-23 16:57:46 +0200

    Plot susy plots 2d as well

--- plot_susy.py
+++ plot_susy.py
@@ -15,7 +15,22 @@ sizes=[4,4,4,32]
 colors=3
 spin=4
 
-density,sizes=Read.bin_mode_1d(sys.argv[1],sizes,colors,spin)
-t=np.linspace(0,sizes[3],32)
-plt.plot(t,density)
-plt.savefig("./"+sys.argv[1] + ".png",dpi=150, bbox_inches='tight')
+if sys.argv[2]:
+    sizes=[16,8,8,8]
+    density,sizes=Read.bin_mode(sys.argv[1],sizes,colors,spin)
+    density_2d=density.sum(axis=(1,2))
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    X=np.arange(0,sizes[3])
+    Y=np.arange(0,sizes[0])
+    
+    X, Y = np.meshgrid(X, Y)
+    
+    ax.plot_surface(X,Y,density_2d, cmap="viridis")
+    plt.savefig("./"+sys.argv[1] + ".png",dpi=150, bbox_inches='tight')
+    
+else:
+    density,sizes=Read.bin_mode_1d(sys.argv[1],sizes,colors,spin)
+    t=np.linspace(0,sizes[3],32)
+    plt.plot(t,density)
+    plt.savefig("./"+sys.argv[1] + ".png",dpi=150, bbox_inches='tight')
