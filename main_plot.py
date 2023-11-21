many changes added, plotting of modes, computation of all the modes added at each step of the cut. Fixed bug with sum of modes when the threshold is lower

main_plot.py
@@ -7,68 +7,92 @@ import Plot_generator
 plt.rcParams.update({'font.size': 16})
 plt.rcParams['text.usetex'] = False
 
-#Param and definitions
-folder_modes=str(sys.argv[1]) #./b2p44_new/ 
-tau_compare=str(sys.argv[2]) # 1.5
-folder_out=str(sys.argv[3]) #./compare_1p5t/         
-
-sizes=[4,4,4,32]
-max_modes=8
+#Definitions parameters
 colors=3
 spin_length=4
-
-lambda_min=0.001
-lambda_max=0.1
-steps=3
-lambdas=np.linspace(lambda_min,lambda_max,num=steps)
 RPO_threshold=0.15
+folder_gf="./gf/"
 
-conf_start=10
-conf_end=1000
-conf_step=10
-configurations=np.arange(conf_start,conf_end,conf_step)
-folder_gf="/beegfs/mi37fud/4x4x4x32_su2/b2p44_new/gf/"
-top_gauge,conf_read=analyzer.Count_index_gf(folder_gf,configurations)
+
+#Param read from screen
+folder_modes=str(sys.argv[1]) #./b2p44_new/ 
+tau_compare=str(sys.argv[2]) # 1.5
+folder_out=str(sys.argv[3]) #./compare_1p5t/   
+operator=str(sys.argv[4]) 
+
+#Param read from file
+f=open(str(sys.argv[3])+"main_parameters.txt", 'r')
+sizes_str=f.readline().replace("\n","").split(" ")
+sizes=[int(element) for element in sizes_str]
+max_modes=int(f.readline())
+method=f.readline().replace("\n","")
+lambda_min=float(f.readline())
+lambda_max=float(f.readline())
+steps=int(f.readline())
+lambdas=np.linspace(lambda_min,lambda_max,num=steps)
+
+conf_start=int(f.readline())
+conf_end=int(f.readline())
+conf_step=int(f.readline())
+conf=np.arange(conf_start,conf_end,conf_step)
+f.close() 
+
+top_gauge,conf_read=analyzer.Count_index_gf(folder_gf,conf)
 print(len(conf_read))
 
 #Plots about GM
 f=open(folder_out+"measures.txt", 'r')
-measures=f.readline().replace("\n","").split(" ")
-time_measures=f.readline().replace("\n","").split(" ")
+measures=f.readline().replace(" \n","").split(" ")
+time_measures=f.readline().replace(" \n","").split(" ")
 print(measures)
 print(time_measures)
-file_measures.close()
+f.close()
 
 observable="GM"
+print("Monte carlo history of Xi")
+print(folder_out)
 Plot_generator.MC_history(folder_out,folder_out,measures,lambdas,observable,Plot=True)
+print("Xi dependence with cut")
 Plot_generator.Cut_dependence(folder_out,folder_out,measures,observable)
-
+print(folder_out)
 #Plot with the GF
 t_start=0
 t_end=1.5
 t_step=0.25
+print("GF vs AFM")
+print(folder_out)
 Plot_generator.GF_vs_AFM(folder_out, folder_gf, folder_out, conf_read, t_start, t_end, t_step,
                          RPO_threshold,tau_compare,measures,time_measures,observable)
 
 for measure in measures:
-    susy_read_s0, susy_read_s1 = analyzer.Count_index_all("./"+measure,"",1000,conf_read,max_modes)
-    Plot_generator.histogram(folder_out+measure,"GM_doublers", conf_read, max_modes,susy_read_s0, susy_read_s1)
+    print(measure)
+    #susy_read_s0, susy_read_s1 = analyzer.Count_index_cut("./"+measure,"",1000,conf_read,max_modes,operator) #Just to have the maximum number of modes
+    print("histogram doublers all modes")
+    #Plot_generator.histogram(folder_out+measure,"GM_doublers", conf_read, max_modes,susy_read_s0, susy_read_s1)
     f=open(folder_out+measure+"lambda_opt.txt",'r') #mesure[0] to take the threshold at 0p5t
     lamba_string=f.read().split('\n')
     lambda_opt,index_opt=float(lamba_string[0]), int(float(lamba_string[1]))
     f.close()
-    print(measure)
-    susy_read_s0, susy_read_s1 = analyzer.Count_index_all("./"+measure,"",lambda_opt,conf_read,max_modes)
-    Plot_generator.histogram(folder_out+measure,"GM_doublers_cut", conf_read, max_modes,susy_read_s0, susy_read_s1)
-    print(folder_out+measure)
+    if method=="cut":
+        susy_read_s0,susy_read_s1,start_spectrum = analyzer.Count_index_cut("./"+measure,"",lambda_opt,conf_read,max_modes,operator)
+    elif method=="gap":
+        susy_read_s0,susy_read_s1 = analyzer.Count_index_gap("./"+measure,"",lambda_opt,conf_read,max_modes,operator)
+    else:
+        print("method "+ method +"not in list")
+    #print("histogram doublers cut")
+    #Plot_generator.histogram(folder_out+measure,"GM_doublers_cut", conf_read, max_modes,susy_read_s0, susy_read_s1)
+    print("susy modes at cut")
     Plot_generator.susy_plot(folder_modes+measure,folder_out+measure,sizes,colors,
-                             spin_length,max_modes,conf_read,susy_read_s0,susy_read_s1)
-
-
-measure=measures[0]
-f=open(folder_out+measure+"lambda_opt.txt",'r') #mesure[0] to take the threshold at 0p5t
-lamba_string=f.read().split('\n')
-lambda_opt,index_opt=float(lamba_string[0]), int(float(lamba_string[1]))
-susy_read_s0, susy_read_s1 = analyzer.Count_index_all("./"+measure,"",lambda_opt,conf_read,max_modes)
-Plot_generator.susy_plot(folder_modes+measure,folder_out+measure,sizes,colors,
-                             spin_length,max_modes,conf_read,susy_read_s0,susy_read_s1)
+                             spin_length,max_modes,conf_read,susy_read_s0,susy_read_s1,operator,"optimal")
+    cut_i=0
+    for cut in lambdas:
+        print("susy modes at ", str(cut))
+        lambda_opt,index_opt=float(cut), cut_i
+        if method=="cut":
+            susy_read_s0,susy_read_s1,start_spectrum = analyzer.Count_index_cut("./"+measure,"",lambda_opt,conf_read,max_modes,operator)
+        if method=="gap":
+            susy_read_s0,susy_read_s1 = analyzer.Count_index_gap("./"+measure,"",lambda_opt,conf_read,max_modes,operator)
+            Plot_generator.susy_plot(folder_modes+measure,folder_out+measure,sizes,colors,
+                                     spin_length,max_modes,conf_read,susy_read_s0,susy_read_s1,operator,str(cut_i))
+        cut_i+=1
+    

mode_plot.py
@@ -7,21 +7,23 @@ import sys
 Sum_over=(0,1,2)
 Mode = str(sys.argv[1])
 Bin = int(sys.argv[2])
-Chirality= 1
+Chirality= int(sys.argv[3])
 colors = 3
 spin_length=4
 
 if Bin:
-    sizes=[8,8,8,64]
+    sizes=[4,4,4,32]
     density_mode,sizes=Read.bin_mode(Mode,sizes,colors,spin_length)
+    #Mode=Mode.replace("./SusyMode_bin_","")
+    #Mode=Mode.split("-")
+    #file_name="SusyMode_"+Mode[1]+"_"+Mode[0]
+    file_name=Mode
 else:
-    density_mode,sizes=Read.ascii_mode(Mode)  
+    zero_mode,density_mode,sizes=Read.ascii_mode(Mode)  
+    file_name=Mode
 
 density_mode=Chirality*density_mode.sum(axis=Sum_over)
 
 t=np.linspace(0,sizes[3],sizes[3])
 plt.plot(t,density_mode)
-Mode.replace("SusyMode_bin_","")
-Mode.split("-")
-file_name="SusyMode_"+Mode[1]+"_"+Mode[0]
-plt.savefig(Mode+".png",dpi=150, bbox_inches='tight')
+plt.savefig(file_name+".png",dpi=150, bbox_inches='tight')